42beede031b69151f473a38457a89cc0c0e64223
OaYenn <amy350500@gmail.com>
2022-11-17 16:58:55 +0900

    회원조회 기능 구현

--- goodbyelion10th/src/main/java/com/example/goodbyelion10th/controller/UserController.java
+++ goodbyelion10th/src/main/java/com/example/goodbyelion10th/controller/UserController.java
@@ -4,8 +4,11 @@ import com.example.goodbyelion10th.domain.Users;
 import com.example.goodbyelion10th.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -21,6 +24,7 @@ public class UserController {
         return "users/createUserForm.html";
     }
 
+    // 회원 등록
     @PostMapping("/users/new")
     public String create(UserForm form) {
         Users user = new Users();
@@ -33,5 +37,14 @@ public class UserController {
         System.out.println("userId = " + userId);
 
         return "redirect:/home";
+    }
+
+    // 회원 조회
+    @GetMapping("/users/list")
+    public String list(Model model) {
+        List<Users> userList = userService.findUsers();
+        model.addAttribute("userList", userList); // view에 (key, value) 쌍으로 데이터 전달
+
+        return "users/userList.html";
     }
 }
